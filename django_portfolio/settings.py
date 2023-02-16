1d13a773c96b4998594364ce722937d4b58db21d
Andrea Elbittar <andreaelbittarl@gmail.com>
2023-02-16 16:07:11 -0500

    creating_static_folder

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -122,7 +122,9 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 MEDIA_URL = '/media/'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
